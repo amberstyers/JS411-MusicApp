d068e75ae0de7f0c858bdd4f39a8fa3058b359c1
amberstyers <amberstyers@gmail.com>
2020-03-11 20:49:53 -0500

    changing stuff around in app.js

--- src/App.js
+++ src/App.js
@@ -1,76 +1,26 @@
-// import React, { Component } from 'react'
-// import NavBar from './components/NavBar'
-// import LogIn from './components/LogIn'
-// import Dashboard from './components/Dashboard'
+import React, { Component } from 'react'
+import NavBar from './components/NavBar'
+import LogIn from './components/LogIn'
+import Dashboard from './components/Dashboard'
+import Button from '@material-ui/core/Button'
 // import { homedir } from 'os';
 
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <NavBar />
-//         <LogIn />
-//         <Dashboard/>
-//       </div>
-//     )
-//   }
-// }
-// export default App
-
-import React, { Component } from 'react'
-import NavBar from './components/NavBar'
-import Button from '@material-ui/core/Button'
-import LogIn from './components/LogIn'
-import Dashboard from './components/Dashboard'
-import './App.css';
-
-
 class App extends Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        loggedIn: true,
-      }
-    }
-
-    logged = (e) => {
-        this.setState({
-          loggedIn: false
-        })
-    }
-
   render() {
-
-    //conditional return based on logged in status
-    if (this.state.loggedIn) {
-      return (
-      <>
-      <NavBar />
-      <br/>
-      <div className = 'dashboardContainer'>
-      <LogIn />
-      <Button variant="contained" color="primary" onClick = {this.logged}>
-        Log-In
-      </Button>
-      </div>
-      </>)
-
-    } else {
-
-      return (
-        <>
+    return (
+      <div>
         <NavBar />
-        <br/>
-        <Dashboard />
-        </>
-      )
-    }
+        <LogIn />
+
+        <Button onClick={this.loggedIn} variant="contained" color="primary">Log In</Button>
+        {/* <Dashboard/> */}
+      </div>
+    )
   }
-
 }
+export default App
 
-export default App 
 
 
 
